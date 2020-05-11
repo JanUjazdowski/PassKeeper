ee9b6c6139817650ce524f6c9e0a428fbdad4e6f
Jan Ujazdowski <ujan326@gmail.com>
2020-05-11 18:07:16 +0200

    Initial commit

---
+++ src/main/java/com/ujo/passkeeper/PassKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.ujo.passkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujo/passkeeper/PassKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujo.passkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
